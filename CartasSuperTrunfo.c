bfdf1eccaa2ee0a9353ce34e0855a28c4cfd61f4
raissa-rgb <raissaferreiraborges7@gmail.com>
2026-04-09 01:53:44 +0000

    finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,45 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
+// Desafio Super Trunfo em c - Países - Nível Novato
+// Estado: ES , Cidade: Vila Velha , Código E01.  Estado: ES , Cidade: Vitória , Código E02.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+ 
+  char estado, codigodacarta[20], cidade[20];
+  int população, pontoturístico;
+  float pib, área;
 
-  // Área para entrada de dados
+   printf (" Digite a inicial do estado: \n");
+scanf (" %c" , &estado);
 
-  // Área para exibição dos dados da cidade
+printf ("Digite o código da carta: \n");
+scanf ("%s" , codigodacarta);
+
+printf ("Digite sua cidade: \n");
+scanf ("%s" , cidade);
+
+printf ("Digite o número de habitantes: \n");
+scanf ("%i" , &população);
+
+printf ("Número aproximado de pontos turísticos: \n");
+scanf ("%d" , &pontoturístico);
+
+printf (" Qual o PIB dessa cidade? \n");
+scanf ("%f" , &pib);
+
+printf ("Digite a área: \n");
+scanf("%f" , &área);
+
+
+printf ("A inicial do estado é:%c \n", estado);
+printf ("O código da carta é:%s \n", codigodacarta);
+printf ("O nome da cidade é:%s \n", cidade);
+printf ("Números de habitantes: %i \n" , população);
+printf ("Número aproximado de pontos turísticos: %d \n" , pontoturístico);
+printf ("PIB: %f \n" , pib);
+printf ("A área é: %f \n" , área);
+
+
 
 return 0;
 } 
